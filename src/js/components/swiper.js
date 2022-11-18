c17123fb8cd3c05be1861ad2b8d21a96d16bdba1
ars3nka <pznkcommercial@gmail.com>
2022-11-18 13:31:29 +0300

    fix bugs, add swipe
    подключил js через модуль, добавил pointer-events, добавил возможность переключать слайды свайпом.
    
    насчёт клавиш которые слетали, добавил им таймер, другого решения пока не нашел

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,4 +1,5 @@
 const images = document.querySelectorAll('.swiper__line img');
+const swiper = document.querySelector('.swiper');
 const swiperLine = document.querySelector('.swiper__line');
 const pagination = document.querySelectorAll('.swiper__pagination_item');
 const swiperButtons = document.querySelector('.swiper__buttons');
@@ -10,43 +11,43 @@ let width = 0;
 
 function resizeSwiper() {
   console.log('resize swiper');
-  width = document.querySelector('.swiper').offsetWidth;
+  width = swiper.offsetWidth;
   swiperLine.style.width = width * images.length + 'px';
   images.forEach((image) => {
     image.style.width = width + 'px';
     image.style.height = 'auto';
   });
-  console.log(document.querySelector('.swiper__line').clientHeight);
-  swiperButtons.style.bottom = ((swiperLine.offsetHeight + swiperPrevBtn.offsetHeight)  / 2) + 'px';
+  buttonsPositionSwiper();
   rollSwiper();
+  setTimeout(buttonsPositionSwiper, 300);
 }
 
-window.addEventListener('resize', resizeSwiper);
+function rollSwiper() {
+  swiperLine.style.transform = 'translate(-'+count*width+'px)';
+  buttonsPositionSwiper();
+}
 
-resizeSwiper();
+function swiperLeft() {
+  count--;
+    if (count < 0) {
+      count = images.length - 1;
+    }
+    rollSwiper();
+    paginationActive();
+}
 
-
-
-swiperNextBtn.addEventListener('click', () => {
+function swiperRight() {
   count++;
   if (count >= images.length) {
     count = 0;
   }
-  rollSwiper();
-  paginationActive();
-});
+}
 
-swiperPrevBtn.addEventListener('click', () => {
-  count--;
-  if (count < 0) {
-    count = images.length - 1;
-  }
-  rollSwiper();
-  paginationActive();
-});
-
-function rollSwiper() {
-  swiperLine.style.transform = 'translate(-'+count*width+'px)';
+function buttonsPositionSwiper() {
+  // console.log(document.querySelector('.swiper__line').clientHeight);
+  // console.log(swiperLine.offsetHeight);
+  // console.log(images[1].clientHeight);
+  swiperButtons.style.bottom = ((swiperLine.offsetHeight + swiperPrevBtn.offsetHeight)  / 2) + 'px';
 }
 
 function paginationActive() {
@@ -56,6 +57,22 @@ function paginationActive() {
   pagination[count].classList.add('active');
 }
 
+resizeSwiper();
+
+window.addEventListener('resize', resizeSwiper);
+
+swiperNextBtn.addEventListener('click', () => {
+  swiperRight();
+  rollSwiper();
+  paginationActive();
+});
+
+swiperPrevBtn.addEventListener('click', () => {
+  swiperLeft();
+  rollSwiper();
+  paginationActive();
+});
+
 pagination.forEach((item, index) => {
   item.addEventListener('click', (event) => {
     count = index;
@@ -63,4 +80,54 @@ pagination.forEach((item, index) => {
     paginationActive();
   });
 });
+
+swiper.addEventListener('touchstart', handleTouchStart, false);
+// swiper.addEventListener('touchmove', handleTouchMove, false);
+swiper.addEventListener('touchend', haldleTouchEnd, false);
+
+let x1 = null;
+let x2 = null;
+let x3 = null;
+
+function handleTouchStart(event) {
+  x1 = event.touches[0].clientX;
+}
+
+// function handleTouchMove(event) {
+//   let x2 = event.touches[0].clientX;
+//   // console.log(x2);
+//   let xDiff = x2 - x1;
+//   // console.log(xDiff);
+//   if (xDiff > 0) {
+//     // console.log('пред слайд *вправо');
+//     console.log(xDiff);
+//     swiperLine.style.transform = `translate(${swiperLine.clientHeight+(xDiff/4)}px)`;
+//   } else {
+//     // console.log('след слайд *влево');
+//     console.log(xDiff);
+//     swiperLine.style.transform = `translate(-${swiperLine.clientHeight+(xDiff/4)}px)`;
+//   }
+//   x2 = null;
+// }
+
+function haldleTouchEnd(event) {
+  rollSwiper();
+  x3 = event.changedTouches[0].clientX;
+
+  let xDiff = x3 - x1;
+
+  if (xDiff < -(width/4)) {
+    swiperRight();
+    rollSwiper();
+    paginationActive();
+  } else if (xDiff > (width/4)){
+    swiperLeft();
+    rollSwiper();
+    paginationActive();
+  }
+  x1 = null;
+  x2 = null;
+  x3 = null;
+}
+
 
