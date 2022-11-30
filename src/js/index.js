6d6b587ff9ac4624067a25684cecc0974eee8e52
Artur <arturio06@gmail.com>
2022-11-30 07:38:03 +0200

    add slider for popUp cards

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,6 @@ import './components/search';
 import './Cards/Card.js';
 import './modal/modal_login';
 import './Cards/addedToBasket.js';
-import modalBasket from './modal/modal_basket.js'
+import modalBasket from './modal/modal_basket.js';
 
 modalBasket();
